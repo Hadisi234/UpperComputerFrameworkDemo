927596eae56142655bf7ea859e029e785460705a
YuanBao <2695002470@qq.com>
2024-03-12 17:05:32 +0800

    添加项目文件。

---
+++ Monitors/PLCMonitor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 上位机框架Demo.Monitors
+{
+    internal class PLCMonitor
+    {
+    }
+}
---
+++ Services/StartService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 上位机框架Demo.Services
+{
+    static class StartService:LogicalStep
+    {
+
+    }
+    /// <summary>
+    /// 抽象共用的流程步
+    /// </summary>
+    public abstract class LogicalStep
+    {
+        public virtual void Start()
+        {
+
+        }
+    }
+}
